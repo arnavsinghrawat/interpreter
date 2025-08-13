8e5dc0c807502c1fe023d5ea1f7115cf7e381ec5
arnavsinghrawat <arnavrawat76@gmail.com>
2025-08-13 19:20:59 +0530

    types of token added

---
+++ token/token.go
@@ -0,0 +1,39 @@
+package token
+
+type TokenType string
+
+type Token  struct{
+	Type TokenType
+	Literal string
+}
+
+const (
+	//Special types
+	ILLEGAL = "ILLEGAL" // To signify a illegal token
+	EOF = "EOF"// To signify "end of line"
+
+
+	//Identifirers+ literals
+	IDENT = "IDENT"
+	INT = "INT"
+
+
+	//Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+
+	//Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+
+	//Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
