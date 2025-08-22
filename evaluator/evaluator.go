c9ba5c91b2e4b78bab2ad603880eefe1df696a29
arnavsinghrawat <arnavrawat76@gmail.com>
2025-08-22 23:59:11 +0530

    Evaluation for integer,expression and program added

---
+++ evaluator/evaluator.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"monkey/ast"
+	"monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatments(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+// since the root ast node is program and you have to travel the stmts
+func evalStatments(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
