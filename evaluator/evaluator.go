cc6ea20fb0201c02ee758e30c60fbd943a2620e9
arnavsinghrawat <arnavrawat76@gmail.com>
2025-08-24 18:41:54 +0530

    evaluter is implmented completely

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -62,6 +62,25 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return val
 		}
 		env.Set(node.Name.Value,val)
+	
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+
+		return &object.Function{Parameters: params, Body: body, Env: env}
+
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]){
+			return args[0]
+		}
+
+		return applyFunction(function, args)
 	}
 
 	return nil
@@ -260,3 +279,53 @@ func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object
 
 	return val
 }
+
+	// to eval the function arguments
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object{
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+
+		if isError(evaluated){
+			return []object.Object{evaluated}
+		}
+
+		result = append(result, evaluated)
+	} 
+
+	return result
+}
+
+
+// for new environment 
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function); if !ok{
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+
+	return unwrapReturnValue(evaluated)
+} 
+
+// for copying old mapping to newer env mapping
+func extendFunctionEnv(fn *object.Function, args[]object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramsIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramsIdx])
+	}
+
+	return env
+}
+
+// this function is used unwrap the return object since we only want to stop for the return of the current scope not all the scope
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok{
+		return returnValue.Value
+	}
+	return obj
+}
+
